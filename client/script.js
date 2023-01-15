2974103aaa0052b98c7520af8f325a368662d0df
Nitsua0615 <45273698+Nitsua0615@users.noreply.github.com>
2023-01-15 13:16:41 -0500

    Revert "Replace depracted scriptProcessor recorder with MediaRecorder"

--- client/script.js
+++ client/script.js
@@ -3801,52 +3801,60 @@ $(function () {
 
 
 
-  // record
+  // record mp3
   (function () {
-    const button = document.querySelector("#record-btn");
-    const audio = MPP.piano.audio;
-    const context = audio.context;
-    let recording = false;
-    const mediaStreamDest = context.createMediaStreamDestination();
-    const recordMimeType = 'audio/ogg; codecs=opus';
-    const recorder = new MediaRecorder(mediaStreamDest.stream, {
-      mimeType: recordMimeType,
-      audioBitsPerSecond: 128000
-    });
-
-    let recordBlob;
-    recorder.ondataavailable = function(chunk){
-      recordBlob = new Blob([recordBlob, chunk.data], {type: recordMimeType});
-    }
-    recorder.onstop = function(){
-      const recordingURL = URL.createObjectURL(recordBlob);
-
-      const recordingNumber = parseInt(window.localStorage.getItem('recordingNumber') ?? 0) + 1;
-      window.localStorage.setItem('recordingNumber', recordingNumber);
-      const recordingFilename = `MultiplayerPiano Recording ${recordingNumber}.ogg`;
-
-      new Notification({ "id": "mp3", "title": "Recording finished", "html": `<a href="${recordingURL}" download="${recordingFilename}">And here it is!</a>`, "duration": 0 });
-    }
-
+    var button = document.querySelector("#record-btn");
+    var audio = MPP.piano.audio;
+    var context = audio.context;
+    var encoder_sample_rate = 44100;
+    var encoder_kbps = 128;
+    var encoder = null;
+    var scriptProcessorNode = context.createScriptProcessor(4096, 2, 2);
+    var recording = false;
+    var recording_start_time = 0;
+    var mp3_buffer = [];
     button.addEventListener("click", function (evt) {
       if (!recording) {
         // start recording
-        recordBlob = new Blob([], {type: recordMimeType});
-        audio.limiterNode.connect(mediaStreamDest);
-        recorder.start();
+        mp3_buffer = [];
+        encoder = new lamejs.Mp3Encoder(2, encoder_sample_rate, encoder_kbps);
+        scriptProcessorNode.onaudioprocess = onAudioProcess;
+        audio.masterGain.connect(scriptProcessorNode);
+        scriptProcessorNode.connect(context.destination);
+        recording_start_time = Date.now();
         recording = true;
         button.textContent = "Stop Recording";
         button.classList.add("stuck");
-        new Notification({ "id": "mp3", "title": "Recording...", "html": "It's recording now.", "duration": 10000 });
+        new Notification({ "id": "mp3", "title": "Recording MP3...", "html": "It's recording now.  This could make things slow, maybe.  Maybe give it a moment to settle before playing.<br><br>This feature is experimental.", "duration": 10000 });
       } else {
         // stop recording
-        recorder.stop();
-        audio.limiterNode.disconnect(mediaStreamDest);
+        var mp3buf = encoder.flush();
+        mp3_buffer.push(mp3buf);
+        var blob = new Blob(mp3_buffer, { type: "audio/mp3" });
+        var url = URL.createObjectURL(blob);
+        scriptProcessorNode.onaudioprocess = null;
+        audio.masterGain.disconnect(scriptProcessorNode);
+        scriptProcessorNode.disconnect(context.destination);
         recording = false;
-        button.textContent = "Record";
+        button.textContent = "Record MP3";
         button.classList.remove("stuck");
+        new Notification({ "id": "mp3", "title": "MP3 recording finished", "html": "<a href=\"" + url + "\" target=\"blank\">And here it is!</a> (open or save as)<br><br>This feature is experimental.", "duration": 0 });
       }
     });
+    function onAudioProcess(evt) {
+      var inputL = evt.inputBuffer.getChannelData(0);
+      var inputR = evt.inputBuffer.getChannelData(1);
+      var mp3buf = encoder.encodeBuffer(convert16(inputL), convert16(inputR));
+      mp3_buffer.push(mp3buf);
+    }
+    function convert16(samples) {
+      var len = samples.length;
+      var result = new Int16Array(len);
+      for (var i = 0; i < len; i++) {
+        result[i] = 0x8000 * samples[i];
+      }
+      return (result);
+    }
   })();
 
 
